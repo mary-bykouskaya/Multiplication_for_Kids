8532620fbad5ffe7c1031a1a677cd633c100db13
mary-bykouskaya <124048197+mary-bykouskaya@users.noreply.github.com>
2023-02-14 00:31:31 +0200

    Add files via upload
    Modified to comply with the issues

--- main.py
+++ main.py
@@ -2,23 +2,37 @@ from random import seed, randint
 
 if __name__ == '__main__':
     count = 0
-    test_passed = 8
+    test_passed = 6
+    summary = []
+    question_str = ""
+    summary_str = ""
     for i in range(10):
         first = randint(1, 10)
         second = randint(1, 10)
         result = first * second
-        print(f"Question: {first} * {second} = ? ", end="")
+        question_str = f"Q{i+1}: {first} * {second} = ?"
+        print(question_str, end=" ")
         try:
             answer = int(input())
+            summary_str = f"Student Answer: {answer}"
         except ValueError:
             answer = None
         if (answer == result):
-            print("Right!")
+            answer_str = f"Answer: {result}; Status: CORRECT"
+            print(summary_str + "; " + answer_str)
+            summary_str = str(question_str + "; " + summary_str + "; " + answer_str)
+            summary.append(summary_str)
             count += 1
         else:
-            print(f"Wrong! The right answer is {result}")
-    print(f"Right answer is {count}")
+            answer_str = f"Answer: {result}; Status: WRONG"
+            print(summary_str + "; " + answer_str)
+            summary_str = str(question_str + "; " + summary_str + "; " + answer_str)
+            summary.append(summary_str)
+    #print()
     if (count >= test_passed):
-        print("Test is passed! Congratulations!!! ")
+        print('\n' + f"Right answer is {count}. " + "Test is passed! Congratulations!!! ")
     else:
-        print("Test is failed!!! ")
+        print('\n' + f"Right answer is {count}. " +  "Test is failed!!! ")
+    print("*** SUMMARY ***", end='\n')
+    for i in range(10):
+        print(summary[i], end='\n')
